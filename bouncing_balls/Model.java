cd8730426011faee23f3ebfcef71ddc37f1fddca
ryggan <andreas@carlssonfoto.se>
2017-04-25 08:57:38 +0200

    Naive version of bouncing implemented

--- bouncing_balls/Model.java
+++ bouncing_balls/Model.java
@@ -4,11 +4,11 @@ import java.awt.Color;
 
 /**
  * The physics model.
- * 
+ *
  * This class is where you should implement your bouncing balls model.
- * 
+ *
  * The code has intentionally been kept as simple as possible, but if you wish, you can improve the design.
- * 
+ *
  * @author Simon Robillard
  *
  */
@@ -18,52 +18,71 @@ class Model {
     final double DAMPENING = 0.98;
 
     double areaWidth, areaHeight;
-    
+
     Ball [] balls;
 
     Model(double width, double height) {
         areaWidth = width;
         areaHeight = height;
-        
+
         // Initialize the model with a few balls
         int i = 0;
-        balls = new Ball[3];
+        balls = new Ball[2];
         balls[i++] = new Ball(width / 3, height * 0.6, 1.0, 0, 0.4, Color.RED);
         balls[i++] = new Ball(2 * width / 3, height * 0.3, -1.6, 0, 0.3, Color.GREEN);
         // balls[i++] = new Ball(width / 4, height * 0.9, 0.8, 0, 0.2, Color.BLUE);
     }
 
     void step(double deltaT) {
+        double[] xCoords = new double[balls.length];
+        double[] yCoords = new double[balls.length];
+
         // TODO this method implements one step of simulation with a step deltaT
-        for (Ball b : balls) {
+        for (int i=0; i<balls.length; i++) {
+            Ball b = balls[i];
             if (b == null) break;
             // gravitation, with size-tweaking
             b.vy += -G * 0.01;
             // b.vy += -G * deltaT;
 
             // detect collision with the border
-            if (b.x < b.radius && b.vx < 0 
+            if (b.x < b.radius && b.vx < 0
                         || b.x > areaWidth - b.radius && b.vx > 0) {
                 b.vx *= -DAMPENING; // change direction of ball
             }
-            if (b.y < b.radius && b.vy < 0 
+            if (b.y < b.radius && b.vy < 0
                         || b.y > areaHeight - b.radius && b.vy > 0) {
                 b.vy *= -DAMPENING;
             }
-            
+
+            xCoords[i] = b.x;
+            yCoords[i] = b.y;
+
             // compute new position according to the speed of the ball
             b.x += deltaT * b.vx;
             b.y += deltaT * b.vy;
         }
 
+        for (int i=0; i<balls.length; i++) {
+            for (int j=0; j<balls.length; j++) {
+                if (i != j) {
+                    double distance = Math.sqrt(Math.pow((balls[i].x - balls[j].x),2) + Math.pow((balls[i].y - balls[j].y),2));
+                    if (distance <= balls[i].radius + balls[j].radius) {
+                        balls[i].vx = -balls[i].vx;
+                        balls[i].vy = -balls[i].vy;
+                    }
+                }
+            }
+        }
+
         // System.out.println("Ball 0 vertical speed: "+balls[0].vy);
     }
-    
+
     /**
      * Simple inner class describing balls.
      */
     class Ball {
-        
+
         Ball(double x, double y, double vx, double vy, double r, Color c) {
             this.x = x;
             this.y = y;
